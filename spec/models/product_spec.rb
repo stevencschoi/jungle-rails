4d8eb95aa9d5eecdec8263bdf8eb9ecff1ad87c8
Steven Choi <stevencschoi87@gmail.com>
2020-04-06 19:25:03 +0000

    tests passed for product_spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+    it "saves product to database" do
+      category = Category.create(name: "Animals")
+      product = Product.create(name: "cat", price: 5000, quantity: 2, category: category)
+      expect(product).to be_present
+    end
+
+    it "does not save if name is missing" do
+      category = Category.create(name: "Animals")
+      product = Product.create(name: nil, price: 5000, quantity: 2, category: category)
+      expect(product.errors.full_messages).to include "Name can't be blank"
+    end
+    
+    it "does not save if price is missing" do
+      category = Category.create(name: "Animals")
+      product = Product.create(name: "cats", price: nil, quantity: 2, category: category)
+      puts product.errors.full_messages
+      expect(product.errors.full_messages).to include "Price can't be blank"
+    end
+    
+    it "does not save if price is missing" do
+      category = Category.create(name: "Animals")
+      product = Product.create(name: "cats", price: 5000, quantity: nil, category: category)
+      puts product.errors.full_messages
+      expect(product.errors.full_messages).to include "Quantity can't be blank"
+    end
+    
+    it "does not save if category is missing" do
+      category = Category.create(name: "Animals")
+      product = Product.create(name: "cats", price: 5000, quantity: 2, category: nil)
+      puts product.errors.full_messages
+      expect(product.errors.full_messages).to include "Category can't be blank"
+    end
+  end
+end
