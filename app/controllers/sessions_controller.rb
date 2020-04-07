1227d725d28a53088cb83b382e65869b38c3c902
Steven Choi <stevencschoi87@gmail.com>
2020-04-07 15:56:16 +0000

    added testing for home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user = User.authenticate_with_credentials(params[:email], params[:password])
-    # if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
